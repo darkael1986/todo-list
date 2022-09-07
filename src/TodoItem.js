4067851d4189a9c613728c1a758c969f6be693fc
Samuel <rosalinisamuel@gmail.com>
2022-09-07 11:49:05 -0300

    Terminando eventos en item

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+
+  const completeTodo = (msg)=>{  
+    alert(msg);     
+  }
+
+
+  const deleteTodo = (msg)=>{
+    alert(msg);
+  }
+
+
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+      onClick = {()=>completeTodo(`Completaste ${props.text}`)}
+      >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span className="Icon Icon-delete"
+      onClick ={()=> deleteTodo(`Borraste ${props.text}`)}
+      >
         X
       </span>
     </li>
